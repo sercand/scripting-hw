f58088f908212071afe1856f18b43fcf04f1d4d4
Sercan Degirmenci <sercan@otsimo.com>
2016-11-26 13:31:13 +0300

    make comments readable

--- component.py
+++ component.py
@@ -12,8 +12,10 @@ class Component:
 
     def attributes(self):
         """
-        attributes returns a list of attribute names and types for the component. Attributes are used at design time to configure a components behaviour.
-        For example an RSS reader component may get the url of the RSS feed and number of most recent messages to display as attributes. attributes should return [(’url’,’string’),(’msgcount’,’int’)].
+        attributes returns a list of attribute names and types for the component. Attributes are
+         used at design time to configure a components behaviour.
+        For example an RSS reader component may get the url of the RSS feed and number of most 
+        recent messages to display as attributes. attributes should return [(’url’,’string’),(’msgcount’,’int’)].
         """
         pass
 
@@ -33,14 +35,19 @@ class Component:
 
     def methods(self):
         """
-        methods return a list of method calls and their descriptions. The methods define the behaviour of the component at execution time. This way, application can interact with the components. RSS reader can return [(’getpage’, ’Changes␣current␣page␣to␣given␣page␣no’)] so that user can go to arbitrary pages on reader. getpage() should be implemented on the RSS reader component
+        methods return a list of method calls and their descriptions. The methods define the behaviour of the 
+        component at execution time. This way, application can interact with the components. RSS reader 
+        can return [(’getpage’, ’Changes␣current␣page␣to␣given␣page␣no’)] so that user can go to arbitrary
+         pages on reader. getpage() should be implemented on the RSS reader component
     class.
         """
         pass
 
     def execute(self):
         """
-        execute method will result in execution of component body. Result depends on the component type. A web application can generate HTML content where a graph based component gets all of inputs and generate outputs.
-        execute is the basic behaviour of the component on execution time. The application is expected to call execute method of all added components to execute a design.
+        execute method will result in execution of component body. Result depends on the component type. 
+        A web application can generate HTML content where a graph based component gets all of inputs and 
+        generate outputs. execute is the basic behaviour of the component on execution time. The application 
+        is expected to call execute method of all added components to execute a design.
         """
         pass
