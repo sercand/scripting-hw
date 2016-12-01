0bbf7f32a984000085f9fdba72a21b67b3ed4dd4
Sercan Degirmenci <sercan@otsimo.com>
2016-12-01 14:20:57 +0300

    test using new instance method

--- test.py
+++ test.py
@@ -9,9 +9,8 @@ print app.loaded()
 idr = app.addInstance('resize', 0, 'resize_with_value')
 idf = app.addInstance('fx', 1, 'gamma')
 
-app.callMethod(idr, 'set_image_width', 600)
-app.callMethod(idr, 'set_image_height', 600)
-app.callMethod(idf, 'set_gamma', 0.6)
+app.instance(idr)['width'] = 600
+app.instance(idr)['height'] = 600
+app.instance(idf)['adj'] = 0.6
 
 app.execute('sample.jpg', 'sample3.jpg')
-
