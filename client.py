0e94c2ca6a22b2a0d0c8fe2beb54f6ffb681fe1c
Sercan Degirmenci <sercan@otsimo.com>
2016-12-10 21:28:03 +0300

    add functions

--- client.py
+++ client.py
@@ -50,6 +50,38 @@ class Client(object):
     def loaded(self):
         return self.__send__("loaded", {})
 
+    def load(self, compid):
+        return self.__send__('load', {"compid": compid})
+
+    def addInstance(self, componentname, index, method, atts):
+        return self.__send__('add_instance', {"index": index,
+                                              "cmp": componentname,
+                                              "method": method,
+                                              "args": atts})
+
+    def removeInstance(self, cid):
+        return self.__send__('remove_instance', {"component_id": cid})
+
+    def getDesign(self):
+        return self.__send__('get_design', {})
+
+    def setDesign(self, designObj):
+        return self.__send__('set_design', designObj)
+
+    def componentMethods(self, componentname):
+        return self.__send__('component_methods', {"name": componentname})
+
+    def componentAttributes(self, componentname):
+        return self.__send__('component_attributes', {"name": componentname})
+
+    def getImage(self):
+        # TODO
+        return self.__send__('image', {})
+
+    def getImageByUrl(self, url):
+        # TODO
+        return self.__send__('image_by_url', {"url": url})
+
 if __name__ == "__main__":
     logger = logging.getLogger("client")
 
